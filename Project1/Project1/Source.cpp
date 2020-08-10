25d0ec374237765f5da2c964623242bd99c7d744
VIDEN22014 <dxbogdan4@gmail.com>
2020-08-10 15:48:09 +0300

    add map value

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <map>
 #include <set>
 
 using namespace std;
 
-int main() {
-	set<string> str;
-	int operation_size;
-	cin >> operation_size;
-	for (int i = 0; i < operation_size; i++)
-	{
-		string s;
-		cin >> s;
-		str.insert(s);
+set<string> BuildMapValuesSet(const map<int, string>& m) {
+	set<string> value;
+	for (auto i : m) {
+		value.insert(i.second);
 	}
-	cout << str.size() << endl;
-	return 0;
+	return value;
 }
