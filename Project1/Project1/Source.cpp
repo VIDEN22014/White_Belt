8558d3b6e39c0107d851f2485954c21d86e5ec41
VIDEN22014 <dxbogdan4@gmail.com>
2020-08-11 11:46:13 +0300

    added synonims

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -5,10 +5,29 @@
 
 using namespace std;
 
-set<string> BuildMapValuesSet(const map<int, string>& m) {
-	set<string> value;
-	for (auto i : m) {
-		value.insert(i.second);
+int main() {
+	int operation_size;
+	cin >> operation_size;
+	string operation_name;
+	map<string,set<string>> synonims;
+	string word1, word2;
+	for (int i = 0; i < operation_size; i++)
+	{
+		cin >> operation_name;
+		if (operation_name == "ADD") {
+			cin >> word1 >> word2;
+			synonims[word1].insert(word2);
+			synonims[word2].insert(word1);
+		}
+		else if (operation_name == "COUNT") {
+			cin >> word1;
+			cout << synonims[word1].size() << endl;
+		}
+		else if (operation_name == "CHECK") {
+			cin >> word1 >> word2;
+			if (synonims[word1].count(word2)==1) cout << "YES" << endl;
+			else cout << "NO" << endl;
+		}
 	}
-	return value;
+	return 0;
 }
